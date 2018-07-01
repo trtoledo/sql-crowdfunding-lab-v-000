795959ef72ec7f749bb8ed26a3ca3ad4eacbf0be
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-01 16:53:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT name, SUM(pledges.amount) FROM users JOIN pledges ON pledges.project_id = users.id GROUP BY "
+"SELECT name, SUM(pledges.amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY pledges.user_id ORDER BY SUM(pledges.amount), users.name ;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
